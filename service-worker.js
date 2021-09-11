4bd92fa5dbe4d171e6a5fdf42f0b6ce998f2c66e
PacoMojica <pacomojica@hotmail.com>
2021-09-10 23:40:51 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/perceptron/precache-manifest.e85987a286afe8e2fb48ebfa91c1ddeb.js"
+  "/precache-manifest.dc994c6c14d11c53b7ef2c1c9967ac58.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/perceptron/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
